204010f079cc3b14858177335a12fec218248fae
shahedis <shahedi.s@aut.ac.ir>
2021-12-01 09:00:44 -0800

    snackbar added

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ function submit() {
     var name = document.getElementById('name').value;
     var gender = document.querySelector('input[name="gender"]:checked').value;
   
-
     const url = `https://api.genderize.io/?name=${name}`
     fetch(url)
     .then(response => response.json())
     .then(data => {
         if(data.gender === null){
             console.log("error");
+            showSnackbar();
         }
         else{
             document.getElementById("p-gender").textContent= data.gender;
@@ -27,4 +27,9 @@ function save() {
 }
 function clear() {
     alert("clear");
+}
+function showSnackbar() {
+    var snkbr = document.getElementById("snackbar");
+    snkbr.className = "show";
+    setTimeout(function(){ snkbr.className = snkbr.className.replace("show", ""); }, 8000);
 }
