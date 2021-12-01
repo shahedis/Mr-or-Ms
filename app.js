5ac1d986bdfe9ce9862faf1b828b2eb72f7d70af
shahedis <shahedi.s@aut.ac.ir>
2021-12-01 07:46:05 -0800

    add fetch api

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 function submit() {
     var name = document.getElementById('name').value;
     var gender = document.querySelector('input[name="gender"]:checked').value;
-    document.getElementById('prediction').style.display= 'flex' ;
-    document.getElementById("p-gender").textContent= "male";
-    document.getElementById("pp-percent").textContent= 0.33;
+  
+
+    const url = `https://api.genderize.io/?name=${name}`
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        if(data.gender === null){
+            console.log("error");
+        }
+        else{
+            document.getElementById("p-gender").textContent= data.gender;
+            document.getElementById("p-percent").textContent= data.probability;
+            document.getElementById('prediction').style.display= 'flex' ;
+        }
+        
+    })
+    .catch(console.error);
+    
 }
 function save() {
     var name = document.getElementById('name').value;
